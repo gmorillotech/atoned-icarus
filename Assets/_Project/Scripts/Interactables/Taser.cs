84771bd65af055e3ab68feb81d85db1b3461891c
Isabella Rodrigues Quesada <allebasirq@gmail.com>
2026-07-28 09:02:04 -0400

    Taser beam sound and animation

--- Assets/_Project/Scripts/Interactables/Taser.cs
+++ Assets/_Project/Scripts/Interactables/Taser.cs
@@ -5,6 +5,8 @@ public class Taser : MonoBehaviour
     [SerializeField] private float stunRange = 5f;
     [SerializeField] private float stunAngle = 90f;
     [SerializeField] private LayerMask droneLayer;
+    [SerializeField] private LineRenderer taserBeam;
+    [SerializeField] private AudioSource taserAudio;
 
     private Transform playerTransform;
 
@@ -17,6 +19,8 @@ public class Taser : MonoBehaviour
     {
 
         Debug.Log("⚡ TASER ACTIVATED!");
+        taserAudio.pitch = Random.Range(0.9f, 1.1f);
+        taserAudio.Play();
 
         Debug.DrawRay(
             playerTransform.position,
@@ -58,6 +62,9 @@ public class Taser : MonoBehaviour
                 if (drone != null)
                 {
                     drone.stunned = true;
+
+                    ShowBeam(obj.transform.position);
+
                     Debug.Log("Drone stunned!");
                 }
             }
@@ -91,5 +98,42 @@ public class Taser : MonoBehaviour
             playerTransform.position,
             playerTransform.forward * stunRange
         );
+    }
+
+    private void ShowBeam(Vector3 targetPosition)
+    {
+        taserBeam.enabled = true;
+
+        int points = 8;
+        taserBeam.positionCount = points;
+
+        Vector3 start = playerTransform.position;
+        Vector3 direction = targetPosition - start;
+
+        for (int i = 0; i < points; i++)
+        {
+            float t = i / (float)(points - 1);
+
+            Vector3 point = Vector3.Lerp(start, targetPosition, t);
+
+            // Add random electricity movement
+            if (i != 0 && i != points - 1)
+            {
+                point += new Vector3(
+                    Random.Range(-0.2f, 0.2f),
+                    Random.Range(-0.2f, 0.2f),
+                    Random.Range(-0.2f, 0.2f)
+                );
+            }
+
+            taserBeam.SetPosition(i, point);
+        }
+
+        Invoke(nameof(HideBeam), 0.1f);
+    }
+
+    private void HideBeam()
+    {
+        taserBeam.enabled = false;
     }
 }
